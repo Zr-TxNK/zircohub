ef6459b4a22d6645328e8d1e55457e2f4601cae3
TxNK <117640922+Zr-TxNK@users.noreply.github.com>
2023-12-29 23:23:08 +0700

    Update bfZr.lua

--- bfZr.lua
+++ bfZr.lua
@@ -1,260 +1,182 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local lplr = game.Players.LocalPlayer
 
-local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
-})
+local QuestData = loadstring(game:HttpGet("https://raw.githubusercontent.com/eixotic07/Bloxfruit/main/Autofarm%20Data.lua"))()
+local WalkTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/eixotic07/Utils/main/Universal%20Tween.lua"))()
 
---Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
+-- No Cooldown
 
-local Options = Fluent.Options
+local CombatFrameworkOld = require(lplr.PlayerScripts.CombatFramework) 
+require(game.ReplicatedStorage.Util.CameraShaker):Stop()
 
-do
-    Fluent:Notify({
-        Title = "Notification",
-        Content = "This is a notification",
-        SubContent = "SubContent", -- Optional
-        Duration = 5 -- Set to nil to make the notification not disappear
-    })
+local CombatFramework = debug.getupvalues(CombatFrameworkOld)[2]
+game:GetService("RunService").Stepped:Connect(function()
+    CombatFramework.activeController.attacking = false
+	CombatFramework.activeController.increment = 3
+	CombatFramework.activeController.blocking = false
+	CombatFramework.activeController.timeToNextBlock = 0
+	CombatFramework.activeController.timeToNextAttack = 0
+    CombatFramework.activeController.hitboxMagnitude = 54
+end)
 
+-- in-game functions
 
+if game.PlaceId == 2753915549 then
+    World = 1
+elseif game.PlaceId == 4442272183 then
+    World = 2
+elseif game.PlaceId == 7449423635 then
+    World = 3
+end
 
-    Tabs.Main:AddParagraph({
-        Title = "Paragraph",
-        Content = "This is a paragraph.\nSecond line!"
-    })
-
-
-
-    Tabs.Main:AddButton({
-        Title = "Button",
-        Description = "Very important button",
-        Callback = function()
-            Window:Dialog({
-                Title = "Title",
-                Content = "This is a dialog",
-                Buttons = {
-                    {
-                        Title = "Confirm",
-                        Callback = function()
-                            print("Confirmed the dialog.")
-                        end
-                    },
-                    {
-                        Title = "Cancel",
-                        Callback = function()
-                            print("Cancelled the dialog.")
-                        end
-                    }
-                }
-            })
-        end
-    })
-
-
-
-    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
-
-    Toggle:OnChanged(function()
-        print("Toggle changed:", Options.MyToggle.Value)
-    end)
-
-    Options.MyToggle:SetValue(false)
-
-
+function StartQuest(Enemy)
+    Quest_Person, Quest_Data = QuestData.getQuest(Enemy)
     
-    local Slider = Tabs.Main:AddSlider("Slider", {
-        Title = "Slider",
-        Description = "This is a slider",
-        Default = 2,
-        Min = 0,
-        Max = 5,
-        Rounding = 1,
-        Callback = function(Value)
-            print("Slider was changed:", Value)
+    for i,v in pairs(Quest_Data) do
+        if i == "CFramePos" then
+            CFramePos = v
+        elseif typeof(v) == "CFrame" then
+            CFramePos = v
         end
-    })
+    end
 
-    Slider:OnChanged(function(Value)
-        print("Slider changed:", Value)
-    end)
+    if (Vector3.new(CFramePos.X, CFramePos.Y, CFramePos.Z) - lplr.Character.HumanoidRootPart.Position).Magnitude >= 10000 and Quest_Data.Entrance then
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance", Quest_Data.Entrance)
+        task.wait(0.4)
+     else
+        task.wait(1)
+        WalkTween(lplr.Character.HumanoidRootPart, CFramePos, 300)
+        task.wait(0.4)
+        if (Vector3.new(CFramePos.X, CFramePos.Y, CFramePos.Z) - lplr.Character.HumanoidRootPart.Position).Magnitude <= 20 then
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StartQuest", Quest_Data.QuestName, Quest_Data.LevelQuest)
+            task.wait(0.5)
+        end
+    end
+end
 
-    Slider:SetValue(3)
-
-
-
-    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
-        Title = "Dropdown",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = false,
-        Default = 1,
-    })
-
-    Dropdown:SetValue("four")
-
-    Dropdown:OnChanged(function(Value)
-        print("Dropdown changed:", Value)
-    end)
-
-
+function UnEquipWeapon(Weapon)
+    if game.Players.LocalPlayer.Character:FindFirstChild(Weapon) then
+        game.Players.LocalPlayer.Character:FindFirstChild(Weapon).Parent = game.Players.LocalPlayer.Backpack
+    end
+end
     
-    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
-        Title = "Dropdown",
-        Description = "You can select multiple values.",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = true,
-        Default = {"seven", "twelve"},
-    })
+function EquipWeapon(ToolSe)
+    if game.Players.LocalPlayer.Backpack:FindFirstChild(ToolSe) then
+        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(ToolSe))
+    end
+end
 
-    MultiDropdown:SetValue({
-        three = true,
-        five = true,
-        seven = false
-    })
-
-    MultiDropdown:OnChanged(function(Value)
-        local Values = {}
-        for Value, State in next, Value do
-            table.insert(Values, Value)
-        end
-        print("Mutlidropdown changed:", table.concat(Values, ", "))
-    end)
-
-
-
-    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
-        Title = "Colorpicker",
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    Colorpicker:OnChanged(function()
-        print("Colorpicker changed:", Colorpicker.Value)
-    end)
-    
-    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
-
-
-
-    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
-        Title = "Colorpicker",
-        Description = "but you can change the transparency.",
-        Transparency = 0,
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    TColorpicker:OnChanged(function()
-        print(
-            "TColorpicker changed:", TColorpicker.Value,
-            "Transparency:", TColorpicker.Transparency
-        )
-    end)
-
-
-
-    local Keybind = Tabs.Main:AddKeybind("Keybind", {
-        Title = "KeyBind",
-        Mode = "Toggle", -- Always, Toggle, Hold
-        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
-
-        -- Occurs when the keybind is clicked, Value is `true`/`false`
-        Callback = function(Value)
-            print("Keybind clicked!", Value)
-        end,
-
-        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
-        ChangedCallback = function(New)
-            print("Keybind changed!", New)
-        end
-    })
-
-    -- OnClick is only fired when you press the keybind and the mode is Toggle
-    -- Otherwise, you will have to use Keybind:GetState()
-    Keybind:OnClick(function()
-        print("Keybind clicked:", Keybind:GetState())
-    end)
-
-    Keybind:OnChanged(function()
-        print("Keybind changed:", Keybind.Value)
-    end)
-
-    task.spawn(function()
-        while true do
-            wait(1)
-
-            -- example for checking if a keybind is being pressed
-            local state = Keybind:GetState()
-            if state then
-                print("Keybind is being held down")
+function Attack(Enemy)
+    if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible then
+        for i,v in pairs(game.Workspace.Enemies:GetChildren()) do
+            if v:IsA("Model") and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 and v.Name:find(Enemy) then
+                repeat task.wait()
+                    if (v.HumanoidRootPart.Position - lplr.Character.PrimaryPart.Position).Magnitude <= 54 then
+                        CombatFramework.activeController.hitboxMagnitude = 54
+                        game:GetService("VirtualUser"):CaptureController()
+                        game:GetService("VirtualUser"):Button1Down(Vector2.new(1000, 1000))
+                    end
+                    WalkTween(lplr.Character.HumanoidRootPart, CFrame.new(v.HumanoidRootPart.Position.X, v.HumanoidRootPart.Position.Y + 50, v.HumanoidRootPart.Position.Z), 400)
+                until not v.Parent or v.Humanoid.Health <= 0
+            else
+                for i,v in pairs(game.Workspace._WorldOrigin.EnemySpawns:GetChildren()) do
+                    if v.Name:find(Enemy) then
+                        WalkTween(lplr.Character.HumanoidRootPart, v.CFrame, 300) break
+                    end
+                end
             end
-
-            if Fluent.Unloaded then break end
         end
-    end)
-
-    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
-
-
-    local Input = Tabs.Main:AddInput("Input", {
-        Title = "Input",
-        Default = "Default",
-        Placeholder = "Placeholder",
-        Numeric = false, -- Only allows numbers
-        Finished = false, -- Only calls callback when you press enter
-        Callback = function(Value)
-            print("Input changed:", Value)
-        end
-    })
-
-    Input:OnChanged(function()
-        print("Input updated:", Input.Value)
-    end)
+    else
+        StartQuest(Enemy)
+    end
 end
 
 
--- Addons:
--- SaveManager (Allows you to have a configuration system)
--- InterfaceManager (Allows you to have a interface managment system)
-
--- Hand the library over to our managers
-SaveManager:SetLibrary(Fluent)
-InterfaceManager:SetLibrary(Fluent)
-
--- Ignore keys that are used by ThemeManager.
--- (we dont want configs to save themes, do we?)
-SaveManager:IgnoreThemeSettings()
-
--- You can add indexes of elements the save manager should ignore
-SaveManager:SetIgnoreIndexes({})
-
--- use case for doing it this way:
--- a script hub could have themes in a global folder
--- and game configs in a separate folder per game
-InterfaceManager:SetFolder("FluentScriptHub")
-SaveManager:SetFolder("FluentScriptHub/specific-game")
-
-InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-SaveManager:BuildConfigSection(Tabs.Settings)
 
 
-Window:SelectTab(1)
 
-Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
+local Meteor = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Bloxburg = Meteor:CreateWindow({
+    Name = "Meteor Lite : Bloxfruit",
+    LoadingTitle = "Meteor Lite",
+    LoadingSubtitle = "by eixotic07",
+    ConfigurationSaving = { Enabled = true, FolderName = "MeteorLite", FileName = "Bloxburg" }
 })
 
--- You can use the SaveManager:LoadAutoloadConfig() to load a config
--- which has been marked to be one that auto loads!
-SaveManager:LoadAutoloadConfig()
+local Autofarm = Bloxburg:CreateTab("Autofarm")
+
+local SelectedQuest = {Quest = "", Enabled = false, AutoSelect = false, FirstSea = "", SecondSea = ""}
+
+table.insert(QuestData.FirstSea, "First Sea")
+table.insert(QuestData.SecondSea, "Second Sea")
+table.insert(QuestData.ThirdSea, "Third Sea")
+
+FirstSeaD = Autofarm:CreateDropdown({
+    Name = "Quest : FirstSea",
+    Options = QuestData.FirstSea,
+    Flag = "Dropdown1",
+    CurrentOption = "First Sea",
+    Callback = function(Option)
+        SelectedQuest.Quest = Option
+    end,
+})
+
+local SecondSeaD = Autofarm:CreateDropdown({
+    Name = "Quest : SecondSea",
+    Options = QuestData.SecondSea,
+    Flag = "Dropdown2",
+    CurrentOption = "Second Sea",
+    Callback = function(Option)
+        SelectedQuest.Quest = Option
+    end,
+})
+
+local ThirdSeaD = Autofarm:CreateDropdown({
+    Name = "Quest : ThirdSea",
+    Options = QuestData.ThirdSea,
+    Flag = "Dropdown3",
+    CurrentOption = "Third Sea",
+    Callback = function(Option)
+        SelectedQuest.Quest = Option
+    end,
+})
+
+
+Autofarm:CreateToggle({ Name = "Auto Select", CurrentValue = false, Flag = "Toggle2",
+    Callback = function(Value)
+        SelectedQuest.AutoSelect = Value
+        
+        while task.wait() do
+            if not SelectedQuest.AutoSelect then break end
+
+            SelectedEnemy = QuestData.CalculateLevel(tonumber(game.Players.LocalPlayer.PlayerGui.Main.Level.Text:sub(5)))
+
+            for i,v in pairs(QuestData.Quests) do
+                if v.EnemyName == SelectedEnemy then
+                    if v.World == 1 and World == 1 then
+                        FirstSeaD:Set(v.EnemyName)
+                    elseif v.World == 2 and World == 2 then
+                        SecondSeaD:Set(v.EnemyName)
+                    elseif v.World == 3 and World == 3 then
+                        ThirdSeaD:Set(v.EnemyName)
+                    end
+                end
+            end
+            
+        end
+    end
+})
+
+Autofarm:CreateSection("Quest Autofarm")
+
+Autofarm:CreateToggle({ Name = "Quest Autofarm", CurrentValue = false, Flag = "Toggle1",
+    Callback = function(Value)
+        SelectedQuest.Enabled = Value
+
+        while task.wait() do
+            if not SelectedQuest.Enabled then break end
+
+            Attack(SelectedQuest.Quest)
+        end
+    end
+})
